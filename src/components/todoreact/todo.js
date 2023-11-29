d6465e65316495106808a687539df1960d45dbe6
Anshuman Dubey <anshumandubey1609@gmail.com>
2023-11-29 10:37:29 +0530

    hard store functionality

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -1,19 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./style.css";
 
+// get the local Data
+const getLocalData = () => {
+    const lists = localStorage.getItem("mytodolist");
+
+    if(lists){
+        return JSON.parse(lists);
+    }
+    else{
+        return [];
+    }
+};
+
 const Todo = () => {
-    const[inputdata, setInputData] = useState("");
-    const [items, setItems] = useState([]);
+    const [inputdata, setInputData] = useState("");
+    const [items, setItems] = useState(getLocalData());
 
     // add the items function
     const addItem = () => {
-        if(!inputdata){
+        if (!inputdata) {
             alert('pls fill the data')
         }
-        else{
-            setItems([...items, inputdata])
+        else {
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name: inputdata,
+            };
+            setItems([...items, myNewInputData]);
+            setInputData("");
         }
     };
+
+    // how to delete items
+    const deleteItem = (index) => {
+        const updatedItems = items.filter((curElem) => {
+            return curElem.id !== index;
+        });
+        setItems(updatedItems)
+    };
+
+    //remove all elements
+    const removeAll = () => {
+        setItems([]);
+    };
+
+    //adding local storage
+    useEffect(() => {
+        localStorage.setItem("mytodolist", JSON.stringify(items));
+    }, [items]);
 
     return (
         <>
@@ -23,19 +58,19 @@ const Todo = () => {
                         <img src="./images/todo.svg" alt="todologo" />
                         <figcaption>Add your list here ✌</figcaption>
                     </figure>
-                    <div className="addItems">                                                                                                             
+                    <div className="addItems">
                         <input type='text' placeholder='✍ Add Item' className="form-control" value={inputdata} onChange={(event) => setInputData(event.target.value)} />
                         <i className="fa fa-plus add-btn" onClick={addItem} ></i>
                     </div>
                     {/* show our items */}
                     <div className='showItems'>
-                        {items.map((curElem, index) => {
+                        {items.map((curElem) => {
                             return (
-                                <div className="eachItem" key={index}>
-                                    <h3>{curElem}</h3>
+                                <div className="eachItem" key={curElem.id}>
+                                    <h3>{curElem.name}</h3>
                                     <div className="todo-btn">
                                         <i className="far fa-edit add-btn"></i>
-                                        <i className="far fa-trash-alt add-btn"></i>
+                                        <i className="far fa-trash-alt add-btn" onClick={() => deleteItem(curElem.id)}></i>
                                     </div>
                                 </div>
                             );
@@ -44,7 +79,8 @@ const Todo = () => {
 
                     {/* remove all button */}
                     <div className="showItems">
-                        <button className="btn effect04" data-sm-link-text="Remove All">
+                        <button className="btn effect04" data-sm-link-text="Remove All"
+                            onClick={removeAll} >
                             <span>
                                 Check List
                             </span>
