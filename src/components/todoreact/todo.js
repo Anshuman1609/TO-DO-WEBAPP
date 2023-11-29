88022aa831f4eb19dd8c51e9c8bbd49dd6b374eb
Anshuman Dubey <anshumandubey1609@gmail.com>
2023-11-29 09:01:21 +0530

    adding "add" functionality

--- src/components/todoreact/todo.js
+++ src/components/todoreact/todo.js
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./style.css";
 
 const Todo = () => {
-  return (
-    <>
-        <div className="main-div">
-            <div className="child-div">
-                <figure>
-                    <img src="./images/todo.svg" alt="todologo" />
-                    <figcaption>Add your list here ✌</figcaption>
-                </figure>
-                <div className="addItems">
-                    <input type='text' placeholder='✍ Add Item' className="form-control" />
+    const[inputdata, setInputData] = useState("");
+    const [items, setItems] = useState([]);
+
+    // add the items function
+    const addItem = () => {
+        if(!inputdata){
+            alert('pls fill the data')
+        }
+        else{
+            setItems([...items, inputdata])
+        }
+    };
+
+    return (
+        <>
+            <div className="main-div">
+                <div className="child-div">
+                    <figure>
+                        <img src="./images/todo.svg" alt="todologo" />
+                        <figcaption>Add your list here ✌</figcaption>
+                    </figure>
+                    <div className="addItems">                                                                                                             
+                        <input type='text' placeholder='✍ Add Item' className="form-control" value={inputdata} onChange={(event) => setInputData(event.target.value)} />
+                        <i className="fa fa-plus add-btn" onClick={addItem} ></i>
+                    </div>
+                    {/* show our items */}
+                    <div className='showItems'>
+                        {items.map((curElem, index) => {
+                            return (
+                                <div className="eachItem" key={index}>
+                                    <h3>{curElem}</h3>
+                                    <div className="todo-btn">
+                                        <i className="far fa-edit add-btn"></i>
+                                        <i className="far fa-trash-alt add-btn"></i>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+
+                    {/* remove all button */}
+                    <div className="showItems">
+                        <button className="btn effect04" data-sm-link-text="Remove All">
+                            <span>
+                                Check List
+                            </span>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Todo
